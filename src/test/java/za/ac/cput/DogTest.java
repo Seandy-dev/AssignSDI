894d4d247a41d418a91fdcf0efabf0d3b91041e2
Thina <217217095@mycput.ac.za>
2021-03-31 22:37:38 +0200

    Second Commit

--- src/test/java/za/ac/cput/DogTest.java
+++ src/test/java/za/ac/cput/DogTest.java
@@ -1,5 +1,12 @@
 package za.ac.cput;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /*
@@ -10,4 +17,40 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class DogTest {
 
+    private Dog Jack, Tom, Harry;
+
+    @BeforeEach
+    public void setUp(){
+        Jack = new Dog();
+        Tom = new Dog();
+        Harry = Jack;
+    }
+
+    @Test
+    public void testForEquality(){
+        Assertions.assertEquals(Jack, Tom);
+    }
+
+    @Test
+    public void testForIdentity(){
+        Assertions.assertSame(Harry, Jack);
+    }
+
+    @Test
+    public void getName(){
+       org.junit.jupiter.api.Assertions.fail("Make the test false. Not implemented");
+    }
+    
+    @Test
+    @org.junit.jupiter.api.Timeout(value = 1000, unit = NANOSECONDS)
+    public void testTimeout(){
+        System.out.println("Setting the Timeout");
+    }
+
+    @Test
+    @Disabled
+    public  void testDisable(){
+        org.junit.jupiter.api.Assertions.fail("The test should fail. The program is still being developed");
+    }
+    
 }
